5047023d6d89266d82373fd704841da8aeeabc7c
Çağan <caganseyrek@outlook.com.tr>
2024-05-22 17:25:52 +0300

    Updated havale page

--- src/routes/islem/yenihavale/+page.svelte
+++ src/routes/islem/yenihavale/+page.svelte
@@ -5,11 +5,9 @@
     let alicihesapnoinput = "";
     let miktarinput = "";
     export var hesaplar = [];
-    export async function handleSubmit() {
-        if (!gondericihesapinput || !alicihesapnoinput || !miktarinput) {
-            window.alert("Lütfen formu eksiksiz doldurunuz.");
-            return;
-        }
+    export async function handleSubmit(event) {
+        event.preventDefault();
+
         try {
             const response = await fetch(
                 "http://localhost:4000/api/hesap/hesapkontrol",
@@ -19,9 +17,9 @@
                         "Content-Type": "application/json",
                         Auth: `Bearer ${localStorage.getItem("token")}`,
                     },
-                    body: {
+                    body: JSON.stringify({
                         HesapNo: alicihesapnoinput,
-                    },
+                    }),
                 },
             );
             hesaplar = await response.json();
@@ -29,9 +27,28 @@
                 window.alert("Gönderici hesabı bulunamadı.");
                 return;
             }
+            var message = "";
+            const havale = await fetch(
+                "http://localhost:4000/api/islem/yenihavale",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Auth: `Bearer ${localStorage.getItem("token")}`,
+                    },
+                    body: JSON.stringify({
+                        senderemail: gondericihesapinput,
+                        receiverhesapno: alicihesapnoinput,
+                        islemtutar: miktarinput,
+                    }),
+                },
+            );
+            const data = await havale.json();
+            message = havale;
         } catch (error) {
             console.error(error);
         }
+        window.alert(message);
     }
     onMount(async () => {
         try {
@@ -50,7 +67,7 @@
                 window.alert(
                     "Havale yapmanız için hesabınız bulunması gerekmektedir.",
                 );
-                return;
+                throw new Error();
             }
         } catch (error) {
             console.error(error);
@@ -93,6 +110,7 @@
                 id="miktar"
                 name="miktar"
                 placeholder="Havale Miktarı"
+                bind:value={miktarinput}
             />
         </div>
         <button type="submit">Havale Yap</button>
